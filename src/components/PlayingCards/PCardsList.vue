5908423e2d3b9dcc99a8930b4566fd13f439a9a1
luigitarallo <tarallo.gigi@gmail.com>
2023-09-12 19:52:23 +0200

    add axiso and get from api

--- src/components/PlayingCards/PCardsList.vue
+++ src/components/PlayingCards/PCardsList.vue
@@ -1,7 +1,24 @@
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      cards: [],
+    };
+  },
+
+  created() {
+    axios
+      .get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0")
+      .then((response) => {
+        this.cards = response.data.data;
+      });
+  },
+};
 </script>
 
-<template></template>
+<template>
+  <div>Lista carte</div>
+</template>
 
 <style lang="scss" scoped></style>
