df718ba1245cadbd570c96ff2083c75e88517498
luigitarallo <tarallo.gigi@gmail.com>
2023-09-12 23:31:23 +0200

    feat: add v-for to generate card

---
+++ src/components/PlayingCards/CardsList.vue
@@ -0,0 +1,26 @@
+<script>
+import Card from "./Card.vue";
+import { store } from "../../data/store";
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+  components: { Card },
+};
+</script>
+
+<template>
+  <div class="row row-cols-2 row-cols-md-4 row-cols-lg-5 gy-3">
+    <Card
+      v-for="card in store.cards"
+      :key="card.id"
+      :image="card.card_images[0].image_url"
+      :name="card.name"
+      :type="card.type"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
