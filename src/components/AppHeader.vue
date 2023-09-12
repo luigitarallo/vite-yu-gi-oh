e9d794de970887dd4918610c9fce9ec908128216
luigitarallo <tarallo.gigi@gmail.com>
2023-09-12 19:26:55 +0200

    import PCardsList

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,33 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <header class="">
+    <div class="logo mx-4">
+      <img src="logo.png" alt="logo small" />
+    </div>
+    <h1>Yu-Gi-Oh Api</h1>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@use "../../src/assets/scss/style.scss" as *;
+
+header {
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  height: 100px;
+  background-color: white;
+  .logo {
+    width: 80px;
+    img {
+      max-width: 100%;
+    }
+  }
+  h1 {
+    font-size: 4rem;
+  }
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,27 @@
+<script>
+import PCardsList from "./PlayingCards/pCardsList.vue";
+export default {
+  components: { PCardsList },
+};
+</script>
+
+<template>
+  <main class="container mt-4">
+    <select class="form-select" aria-label="Default select example">
+      <option selected>Alien</option>
+      <option value="1">Alien</option>
+    </select>
+    <PCardsList />
+  </main>
+</template>
+
+<style lang="scss" scoped>
+@use "../../src/assets/scss/style.scss" as *;
+
+main {
+  height: 100vh;
+  select {
+    width: 10%;
+  }
+}
+</style>
