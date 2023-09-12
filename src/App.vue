d1b0dfebbc8e777901eba81b587854acf319f3c8
luigitarallo <tarallo.gigi@gmail.com>
2023-09-12 23:31:45 +0200

    feat: import store, axios

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <script>
+import axios from "axios";
+import { store } from "./data/store";
 import AppHeader from "./components/AppHeader.vue";
 import AppMain from "./components/AppMain.vue";
 
 export default {
+  data() {
+    return {
+      store,
+    };
+  },
+
   components: { AppHeader, AppMain },
+
+  methods: {
+    fetchCards(endpoint) {
+      axios.get(endpoint).then((response) => {
+        store.cards = response.data.data;
+      });
+    },
+  },
+
+  created() {
+    this.fetchCards(store.apiUri);
+  },
 };
 </script>
 
