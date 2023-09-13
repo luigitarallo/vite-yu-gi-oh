7bddc8f3b1b6247d76d44f68d8c42ed2d9b362f9
luigitarallo <tarallo.gigi@gmail.com>
2023-09-13 17:57:26 +0200

    feat: add component baseselect

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "Hello world",
+    };
+  },
+};
+</script>
+
+<template>
+  <select
+    @change="$emit('change-select')"
+    class="form-select"
+    aria-label="Default select example"
+  >
+    <option selected>Alien</option>
+    <option value="1">Alien</option>
+  </select>
+</template>
+
+<style lang="scss" scoped></style>
