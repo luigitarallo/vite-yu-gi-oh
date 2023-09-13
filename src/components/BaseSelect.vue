47d84bb3d2a7f7c62a23752195a037ef62dcdc97
luigitarallo <tarallo.gigi@gmail.com>
2023-09-13 21:58:03 +0200

    feat: add selected, vodel

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -1,19 +1,26 @@
 <script>
 export default {
+  data() {
+    return {
+      selected: "",
+    };
+  },
   emits: ["change-select"],
   props: {
     options: Object,
+    placeHolder: String,
   },
 };
 </script>
 
 <template>
   <select
-    @change="$emit('change-select')"
+    v-model="selected"
+    @change="$emit('change-select', selected)"
     class="form-select mt-4"
     aria-label="Default select example"
   >
-    <option selected></option>
+    <option disabled value="">{{ placeHolder }}</option>
     <option v-for="option in options">{{ option.archetype_name }}</option>
   </select>
 </template>
